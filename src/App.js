fb0d8acdcf7a161ebdba1e14ff07d49f6c0559fb
JANANI-HUB <113664724+JANANI-HUB@users.noreply.github.com>
2022-11-21 12:27:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import "./App.css";
 import React from 'react';
-import Page3 from './Page3';
 import Page4 from './Page4';
-import{BrowserRouter,Routes,Route} from "react-router-dom";
+
 function App(){
 return (
-  <BrowserRouter>
-  <div>
-<Routes>
-<Route path='/' element={<Page4/>}></Route>
-  <Route path='/Page3' element={<Page3/>}></Route>
   
-</Routes>
-  </div>
-</BrowserRouter>
+  
+  <Page4/>
 );
 }
-
 export default  App
 
